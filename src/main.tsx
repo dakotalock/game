78d13bff178fe16f1fc3cb6dc2829b6bdf62c980
dakotalock <125934673+dakotalock@users.noreply.github.com>
2024-12-22 19:18:26 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // optional if you want to style things with CSS
+import Game from './app';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
